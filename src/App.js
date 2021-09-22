4f5a05974f197f6e01e97deb555ee741bc577da5
mesu-ai <mesu.s.ss@gmail.com>
2021-09-22 16:33:07 +0600

    react api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <UserDetails></UserDetails>
+
+
     </div>
   );
+}
+
+function UserDetails(props) {
+  const [users,setusers]= useState([]);
+useEffect(()=>{
+
+// console.log('You clicked the button !');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res=>res.json())
+.then(data=>setusers(data));
+
+
+
+},[]);
+
+
+  return(
+    <div>
+
+      <h2>Users Details</h2>
+      {users.map(user=><User name={user.name} email={user.email} phone={user.phone}></User>)}
+
+    </div>
+  )
+  
+}
+
+const User=(props)=>{
+  return(
+    
+       <div className="user">
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+      <p>Phone: {props.phone}</p>
+
+    </div>
+   
+  )
 }
 
 export default App;
